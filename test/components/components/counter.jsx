5a439217486ae2dbdedd65edbe8ceb87e192fe30
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2016-06-28 19:46:24 +0900

    fix bug component click event

--- test/components/components/counter.jsx
+++ test/components/components/counter.jsx
@@ -5,8 +5,8 @@ function Counter(props) {
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={onIncrement()}>+1</button>
-      <button onClick={onDecrement()}>-1</button>
+      <button onClick={onIncrement}>+1</button>
+      <button onClick={onDecrement}>-1</button>
     </div>
   );
 }
