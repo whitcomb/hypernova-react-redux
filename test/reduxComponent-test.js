b484d179fd5df26d87decd3887aec6f787c48456
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2016-06-29 22:55:24 +0900

    change file path

--- test/reduxComponent-test.js
+++ test/reduxComponent-test.js
@@ -1,21 +1,21 @@
-import * as actions from './components/actions/';
-import * as types from './components/constants/ActionTypes';
-import reducer from './components/reducers';
+import * as actions from './components/Actions/Counter';
+import * as types from './components/Constants/ActionTypes/Counter';
+import reducer from './components/Reducers/Counter';
 import jsdom from 'jsdom';
 import { assert } from 'chai';
-//import { renderReact } from '..';
+// import { renderReact } from '..';
 
 describe('[redux] counter actions', () => {
   it('should create an action to increment counter', () => {
     const expectedAction = {
-      type: types.INCREMENT_COUNTER
+      type: types.INCREMENT_COUNTER,
     };
     assert.deepEqual(actions.incrementCounter(), expectedAction);
   });
 
   it('should create an action to decrement counter', () => {
     const expectedAction = {
-      type: types.DECREMENT_COUNTER
+      type: types.DECREMENT_COUNTER,
     };
     assert.deepEqual(actions.decrementCounter(), expectedAction);
   });
@@ -23,20 +23,20 @@ describe('[redux] counter actions', () => {
 
 describe('[redux] counter reducers', () => {
   it('should return the initial state', () => {
-    assert.deepEqual(reducer(undefined, {}), { count: 0 })
-  })
+    assert.deepEqual(reducer(undefined, {}), { count: 0 });
+  });
 
   it('should handle INCREMENT_COUNTER', () => {
     assert.deepEqual(
       reducer(undefined, { type: types.INCREMENT_COUNTER }),
       { count: 1 }
-    )
-  })
+    );
+  });
 
   it('should handle DECREMENT_COUNTER', () => {
     assert.deepEqual(
       reducer(undefined, { type: types.DECREMENT_COUNTER }),
       { count: -1 }
-    )
-  })
-})
+    );
+  });
+});
